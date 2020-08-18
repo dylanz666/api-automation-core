dc420974c3343f24c7c52c8a55cdc3cfa00b5db6
dylanz <zzh9098-=>
2020-08-18 22:09:16 +0800

    Update author in App.java

--- src/main/java/com/github/dylanz666/App.java
+++ src/main/java/com/github/dylanz666/App.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hello world!
- */
+ * @author : dylanz
+ * @since : 08/18/2020
+ **/
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
